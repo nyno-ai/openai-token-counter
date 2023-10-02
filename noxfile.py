b568e0bc5290d9278ebce16c1eade8ad82e2ba6c
Eitan Nargassi <eitan1112@gmail.com>
2023-10-02 21:14:37 +0300

    fix noxfile

--- noxfile.py
+++ noxfile.py
@@ -22,12 +22,7 @@ except ImportError:
 
 
 package = "openai_token_counter"
-python_versions = [
-    "3.12",
-    "3.11",
-    "3.10",
-    "3.9",
-]
+python_versions = ["3.12", "3.11", "3.10", "3.9"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
